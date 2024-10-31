e8eb08e0d385b349ab3ca2990557750fad40c05b
Moreira-Ruan <ruanmoreira.dev@gmail.com>
2024-10-31 20:05:41 -0300

    Upload only Python script and guide

--- video_download.py
+++ video_download.py
@@ -2,9 +2,9 @@ import yt_dlp
 
 # Lista de URLs de vídeos individuais
 video_urls = [
-    'https://www.youtube.com/watch?v=pAEd1qEfCEw&list=PLB9D73ypDbAUpitzq2VOOvxGlPGdzBTMU&index=1',
-    'https://www.youtube.com/watch?v=NtntbPpfqbc&list=PLB9D73ypDbAUpitzq2VOOvxGlPGdzBTMU&index=8',
-    'https://www.youtube.com/watch?v=OGqf-zsZw6s&list=PLB9D73ypDbAUpitzq2VOOvxGlPGdzBTMU&index=4'
+    '',
+    '',
+    ''
 ]
 
 # Diretório de download
